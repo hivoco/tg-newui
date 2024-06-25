f153b1e75e9bb75d985997553434e654c5112904
ranjanhivoco <ranjan@hivoco.com>
2024-06-25 18:02:31 +0530

    new api key add

--- src/components/TextToSpeech.jsx
+++ src/components/TextToSpeech.jsx
@@ -11,7 +11,12 @@ const TextToSpeech = ({
   const wrongOrRightRef = useRef();
 
   const [audioUrl, setAudioUrl] = useState("");
-  const API_KEY =   'sk-proj-BeLQ0uFPzdX6835TeUGCT3BlbkFJKCpNBcXhE63skhEm8yie'
+  // const API_KEY =   'sk-proj-BeLQ0uFPzdX6835TeUGCT3BlbkFJKCpNBcXhE63skhEm8yie'
+  
+  const API_KEY = import.meta.env.VITE_API_KEY
+  // console.log(import.meta.env.VITE_API_KEY) 
+
+  
   const handleTextToSpeech = async () => {
     setAudioUrl("");
 
